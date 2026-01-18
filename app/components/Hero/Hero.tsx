111f7c911b0ece8089d07f37a5fc1d09bb591fdb
Vince Linise <contact@vincelinise.com>
2026-01-18 16:01:46 +0100

    feat: initial commit

---
+++ app/components/Hero/Hero.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import Image from "next/image";
+import "./Hero.css";
+import { motion } from "motion/react";
+import Icons from "./Icons";
+
+export default function Hero() {
+  return (
+    <div className="items-center justify-center flex">
+
+      <div className="absolute icons-mask">
+        <Icons />
+      </div>
+
+      <motion.div className="w-54 h-70 rounded-full relative overflow-hidden"
+        initial={{ scale: 0.5, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 0.4, ease: [0.3, 0, 0, 1] }}
+      >
+        <motion.div
+          initial={{ y: 280 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.8, delay: 0, type: "spring", bounce: 0.25 }}
+          className="absolute top-[10px] h-[120%] min-w-[155%] left-[-30%] z-1"
+        >
+          <Image
+            src="/vince.png"
+            alt=""
+            width={354}
+            height={360}
+          />
+        </motion.div>
+        <motion.div
+          className="pdp-gradient w-54 h-54 rounded-full absolute bottom-0 left-0 z-0"
+        ></motion.div>
+      </motion.div>
+
+    </div>
+  );
+}
